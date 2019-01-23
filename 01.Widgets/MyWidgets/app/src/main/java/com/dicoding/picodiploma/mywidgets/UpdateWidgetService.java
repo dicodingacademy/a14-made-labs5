2cafe6758f75c1079f707a6a42b6af3d5f6c49df
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2019-01-23 17:05:50 +0700

    Update MyWidgets to Android Studio 3.3

--- 01.Widgets/MyWidgets/app/src/main/java/com/dicoding/picodiploma/mywidgets/UpdateWidgetService.java
+++ 01.Widgets/MyWidgets/app/src/main/java/com/dicoding/picodiploma/mywidgets/UpdateWidgetService.java
@@ -11,19 +11,15 @@ import android.widget.RemoteViews;
  */
 
 public class UpdateWidgetService extends JobService {
+
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
         AppWidgetManager manager = AppWidgetManager.getInstance(this);
-
-        RemoteViews view = new RemoteViews(getPackageName(), R.layout.bilangan_acak_widget);
-        ComponentName theWidget = new ComponentName(this, BilanganAcakWidget.class);
-
-        String lastUpdate = "Random: "+NumberGenerator.Generate(100);
-
+        RemoteViews view = new RemoteViews(getPackageName(), R.layout.random_numbers_widget);
+        ComponentName theWidget = new ComponentName(this, RandomNumbersWidget.class);
+        String lastUpdate = "Random: " + NumberGenerator.Generate(100);
         view.setTextViewText(R.id.appwidget_text, lastUpdate);
-
         manager.updateAppWidget(theWidget, view);
-
         jobFinished(jobParameters, false);
         return true;
     }
