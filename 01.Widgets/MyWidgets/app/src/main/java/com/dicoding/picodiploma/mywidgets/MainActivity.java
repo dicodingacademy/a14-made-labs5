7bccaccfbc1830ee8fa89f45d1559a0b5f3929e5
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2019-01-23 16:52:14 +0700

    Update MyWidgets to Android Studio 3.3

--- 01.Widgets/MyWidgets/app/src/main/java/com/dicoding/picodiploma/mywidgets/MainActivity.java
+++ 01.Widgets/MyWidgets/app/src/main/java/com/dicoding/picodiploma/mywidgets/MainActivity.java
@@ -4,24 +4,24 @@ import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnStart;
     Button btnStop;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnStart = (Button)findViewById(R.id.btn_start);
-        btnStop = (Button)findViewById(R.id.btn_stop);
-
+        btnStart = findViewById(R.id.btn_start);
+        btnStop = findViewById(R.id.btn_stop);
 
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
@@ -29,9 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
         int id = view.getId();
-        switch(id){
+        switch (id) {
             case R.id.btn_start:
                 startJob();
                 break;
@@ -40,23 +39,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 cancelJob();
                 break;
         }
-
     }
 
     private static int jobId = 100;
-    private static int PERIODE_SCHEDULER = 86000;
+    private static int SCHEDULE_OF_PERIOD = 2000;
 
-    private void startJob(){
+    private void startJob() {
         ComponentName mServiceComponent = new ComponentName(this, UpdateWidgetService.class);
+
         JobInfo.Builder builder = new JobInfo.Builder(jobId, mServiceComponent);
         builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NONE);
-        builder.setPeriodic(PERIODE_SCHEDULER);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            builder.setMinimumLatency(SCHEDULE_OF_PERIOD);
+        } else {
+            builder.setPeriodic(SCHEDULE_OF_PERIOD);
+        }
         JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
         jobScheduler.schedule(builder.build());
 
         Toast.makeText(this, "Job Service started", Toast.LENGTH_SHORT).show();
     }
-    private void cancelJob(){
+
+    private void cancelJob() {
         JobScheduler tm = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
         tm.cancel(jobId);
         Toast.makeText(this, "Job Service canceled", Toast.LENGTH_SHORT).show();
