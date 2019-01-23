13caa1adc053b222a077fbb5c62e98e1a038225d
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2019-01-23 17:05:50 +0700

    Update MyWidgets to Android Studio 3.3

--- 01.Widgets/MyWidgets/app/src/main/java/com/dicoding/picodiploma/mywidgets/NumberGenerator.java
+++ 01.Widgets/MyWidgets/app/src/main/java/com/dicoding/picodiploma/mywidgets/NumberGenerator.java
@@ -8,10 +8,8 @@ import java.util.Random;
 
 public class NumberGenerator {
 
-    public static int Generate(int max){
-
+    public static int Generate(int max) {
         Random random = new Random();
-        int randomInt = random.nextInt(max);
-        return randomInt;
+        return random.nextInt(max);
     }
 }
