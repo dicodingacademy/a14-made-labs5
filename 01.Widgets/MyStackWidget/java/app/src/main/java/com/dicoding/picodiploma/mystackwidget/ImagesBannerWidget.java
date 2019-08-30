282c14629bb86e2b2a92ac0eedbc9d0a39ffb5e8
Arif <faizin.arief@gmail.com>
2019-08-30 14:23:48 +0700

    Update Java Codacy StackWidget

---
+++ 01.Widgets/MyStackWidget/java/app/src/main/java/com/dicoding/picodiploma/mystackwidget/ImagesBannerWidget.java
@@ -0,0 +1,67 @@
+package com.dicoding.picodiploma.mystackwidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class ImagesBannerWidget extends AppWidgetProvider {
+
+    private static final String TOAST_ACTION = "com.dicoding.picodiploma.TOAST_ACTION";
+    public static final String EXTRA_ITEM = "com.dicoding.picodiploma.EXTRA_ITEM";
+
+    /*
+    Update widget berdasarkan id widget-nya di home screen
+     */
+    private static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        Intent intent = new Intent(context, StackWidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.image_banner_widget);
+        views.setRemoteAdapter(R.id.stack_view, intent);
+        views.setEmptyView(R.id.stack_view, R.id.empty_view);
+
+        Intent toastIntent = new Intent(context, ImagesBannerWidget.class);
+        toastIntent.setAction(ImagesBannerWidget.TOAST_ACTION);
+        toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    /*
+    Update widget
+     */
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    /*
+    Gunakan onReceive untuk menerima broadcast
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction() != null) {
+            if (intent.getAction().equals(TOAST_ACTION)) {
+                int viewIndex = intent.getIntExtra(EXTRA_ITEM, 0);
+                Toast.makeText(context, "Touched view " + viewIndex, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
+
