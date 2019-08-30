e93964dcd4c8eb7a99a5d2a4a21e513b7ce042ae
Arif <faizin.arief@gmail.com>
2019-08-30 14:23:48 +0700

    Update Java Codacy StackWidget

---
+++ 01.Widgets/MyStackWidget/java/app/src/main/java/com/dicoding/picodiploma/mystackwidget/StackWidgetService.java
@@ -0,0 +1,15 @@
+package com.dicoding.picodiploma.mystackwidget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+/**
+ * Created by dicoding on 1/9/2017.
+ */
+
+public class StackWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new StackRemoteViewsFactory(this.getApplicationContext());
+    }
+}
