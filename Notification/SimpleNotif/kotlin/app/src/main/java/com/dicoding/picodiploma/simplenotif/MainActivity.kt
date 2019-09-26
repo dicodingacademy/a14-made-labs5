9aeac2ebe2396270b47c9442686ae5c6a435bebc
Arif <faizin.arief@gmail.com>
2019-09-26 14:58:15 +0700

    Fix SimpleNotif
    back to old

--- Notification/SimpleNotif/kotlin/app/src/main/java/com/dicoding/picodiploma/simplenotif/MainActivity.kt
+++ Notification/SimpleNotif/kotlin/app/src/main/java/com/dicoding/picodiploma/simplenotif/MainActivity.kt
@@ -2,28 +2,38 @@ package com.dicoding.picodiploma.simplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
-
     companion object {
-        val NOTIFICATION_ID = 1
-        var CHANNEL_ID = "channel_01"
-        var CHANNEL_NAME: CharSequence = "dicoding channel"
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_01"
+        private const val CHANNEL_NAME = "dicoding channel"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //aksi untuk onClick pada button
+    fun sendNotification(view: View) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.drawable.ic_notifications_white_48px)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_white_48px))
                 .setContentTitle(resources.getString(R.string.content_title))
@@ -36,9 +46,8 @@ class MainActivity : AppCompatActivity() {
         */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-            val channel = NotificationChannel(CHANNEL_ID,
-                    CHANNEL_NAME,
-                    NotificationManager.IMPORTANCE_DEFAULT)
+            /* Create or update. */
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
 
             mBuilder.setChannelId(CHANNEL_ID)
 
