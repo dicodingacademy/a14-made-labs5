7d13a27ad55c926c41a115f117e82ce7b5d875a5
Arif <faizin.arief@gmail.com>
2019-09-27 10:18:13 +0700

    Fix FCM

--- FCM/MyFirebaseCloudMessaging/java/app/src/main/java/com/dicoding/picodiploma/myfirebasecloudmessaging/MainActivity.java
+++ FCM/MyFirebaseCloudMessaging/java/app/src/main/java/com/dicoding/picodiploma/myfirebasecloudmessaging/MainActivity.java
@@ -23,24 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String channelId = getString(R.string.default_notification_channel_id);
-            String channelName = getString(R.string.default_notification_channel_name);
-            NotificationManager notificationManager =
-                    getSystemService(NotificationManager.class);
-            if (notificationManager != null) {
-                notificationManager.createNotificationChannel(new NotificationChannel(channelId,
-                        channelName, NotificationManager.IMPORTANCE_LOW));
-            }
-        }
-
-        if (getIntent().getExtras() != null) {
-            for (String key : getIntent().getExtras().keySet()) {
-                Object value = getIntent().getExtras().get(key);
-                Log.d(TAG, "Key: " + key + " Value: " + value);
-            }
-        }
-
         Button subscribeButton = findViewById(R.id.btn_subscribe);
         subscribeButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- FCM/MyFirebaseCloudMessaging/kotlin/app/src/main/java/com/dicoding/picodiploma/myfirebasecloudmessaging/MainActivity.kt
+++ FCM/MyFirebaseCloudMessaging/kotlin/app/src/main/java/com/dicoding/picodiploma/myfirebasecloudmessaging/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.dicoding.picodiploma.myfirebasecloudmessaging
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -20,21 +17,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = getString(R.string.default_notification_channel_id)
-            val channelName = getString(R.string.default_notification_channel_name)
-            val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager?.createNotificationChannel(NotificationChannel(channelId,
-                    channelName, NotificationManager.IMPORTANCE_LOW))
-        }
-
-        intent.extras?.let {
-            for (key in it.keySet()) {
-                val value = it.get(key)
-                Log.d(TAG, "Key: $key Value: $value")
-            }
-        }
 
         btn_subscribe.setOnClickListener {
             FirebaseMessaging.getInstance().subscribeToTopic("news")
