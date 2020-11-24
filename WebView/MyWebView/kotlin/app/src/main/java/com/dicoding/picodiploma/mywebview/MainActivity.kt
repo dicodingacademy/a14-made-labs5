52c3eda16da2ed171005cabcc88390ad02d6ac17
Arif <faizin.arief@gmail.com>
2020-11-24 14:09:03 +0700

    Update Android Studio 4.1 - WebView

--- WebView/MyWebView/kotlin/app/src/main/java/com/dicoding/picodiploma/mywebview/MainActivity.kt
+++ WebView/MyWebView/kotlin/app/src/main/java/com/dicoding/picodiploma/mywebview/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.dicoding.picodiploma.mywebview
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
 
         webView.settings.javaScriptEnabled = true
 
